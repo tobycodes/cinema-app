0fd0e4de78301243f317b695a44d231cd2b23600
tobycodes <davidolubusoye@gmail.com>
2021-06-20 14:36:51 +0100

    added header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import './App.css';
+import './App.scss';
 
+import Header from './components/Header';
 import store from './redux/store';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="app">Redux setup complete</div>
+      <Header />
+      <div className="app"></div>
     </Provider>
   );
 }
